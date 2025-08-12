e98f85adec9a17f28ab6724ed1a5bbd01a8f9d22
ian12-21 <iflegar68@gmail.com>
2025-08-12 16:37:45 +0200

    new factory

--- contracts/QuizFactoryV4V5.sol
+++ contracts/QuizFactoryV4V5.sol
@@ -8,13 +8,6 @@ pragma solidity ^0.8.19;
  */
 contract QuizFactory {
     address public owner;
-    address public platformWallet;
-    
-    Quiz[] public basicQuizzes;
-    QuizWithFees[] public paidQuizzes;
-    
-    mapping(address => Quiz[]) public userBasicQuizzes;
-    mapping(address => QuizWithFees[]) public userPaidQuizzes;
     
     event BasicQuizCreated(address indexed quizAddress, address indexed creator, uint256 questionCount);
     event PaidQuizCreated(address indexed quizAddress, address indexed creator, uint256 questionCount, uint256 entryFee);
@@ -22,11 +15,6 @@ contract QuizFactory {
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner can call this");
         _;
-    }
-    
-    constructor(address _platformWallet) {
-        owner = msg.sender;
-        platformWallet = _platformWallet;
     }
 
     /**
@@ -41,9 +29,6 @@ contract QuizFactory {
             questionCount,
             answersHash
         );
-        
-        basicQuizzes.push(quiz);
-        userBasicQuizzes[msg.sender].push(quiz);
         
         emit BasicQuizCreated(address(quiz), msg.sender, questionCount);
         return address(quiz);
@@ -64,42 +49,9 @@ contract QuizFactory {
             questionCount,
             answersHash,
             entryFee,
-            platformWallet
         );
-        
-        paidQuizzes.push(quiz);
-        userPaidQuizzes[msg.sender].push(quiz);
         
         emit PaidQuizCreated(address(quiz), msg.sender, questionCount, entryFee);
         return address(quiz);
-    }
-
-    /**
-     * @dev Get all basic quizzes created by user
-     */
-    function getUserBasicQuizzes(address user) external view returns (Quiz[] memory) {
-        return userBasicQuizzes[user];
-    }
-
-    /**
-     * @dev Get all paid quizzes created by user
-     */
-    function getUserPaidQuizzes(address user) external view returns (QuizWithFees[] memory) {
-        return userPaidQuizzes[user];
-    }
-
-    /**
-     * @dev Get total number of quizzes
-     */
-    function getTotalQuizzes() external view returns (uint256 basic, uint256 paid) {
-        return (basicQuizzes.length, paidQuizzes.length);
-    }
-
-    /**
-     * @dev Update platform wallet (only owner)
-     */
-    function updatePlatformWallet(address newPlatformWallet) external onlyOwner {
-        require(newPlatformWallet != address(0), "Invalid platform wallet");
-        platformWallet = newPlatformWallet;
     }
 }
